91851de3a12dba8499568ffff2bbe5423ce75f9a
Christin Gruber <c.gruber@touchdesign.de>
2010-12-23 20:35:45 +0000

    Set version 1.1, Clear spaces and tabs (clean code indents)

--- function.touchrandimg.php
+++ function.touchrandimg.php
@@ -8,7 +8,7 @@
  *
  * @category Plugin
  * @author Christoph Gruber <www.touchdesign.de>
- * @version 1.0
+ * @version 1.1
  * @copyright touchDesign 28.08.2010
  * @link http://www.touchdesign.de/
  * @link http://www.homepage-community.de/index.php?topic=1705
@@ -40,111 +40,111 @@
  */
 
 function smarty_function_touchrandimg($params, &$smarty) {
-	global $gCms;
+  global $gCms;
 
-	$config = &$gCms->config;
+  $config = &$gCms->config;
 
-	// Grep params
-	$folder = !empty($params['folder'])
-		? $params['folder'] : "touchrandimg";
+  // Grep params
+  $folder = !empty($params['folder'])
+    ? $params['folder'] : "touchrandimg";
 
-	$maxImg = !empty($params['max_img']) 
-		? (int)$params['max_img'] : 1;
+  $maxImg = !empty($params['max_img']) 
+    ? (int)$params['max_img'] : 1;
 
-	$linkMap = !empty($params['link_map']) 
-		? linkmap2array(explode(",",$params['link_map'])) : NULL;
+  $linkMap = !empty($params['link_map']) 
+    ? linkmap2array(explode(",",$params['link_map'])) : NULL;
 
-	$linkTarget = !empty($params['link_target']) 
-		? explode(",",$params['link_target']) : "_self";
+  $linkTarget = !empty($params['link_target']) 
+    ? explode(",",$params['link_target']) : "_self";
 
-	// Set path and url
-	$touchRandImgPath = $config['uploads_path'] . DIRECTORY_SEPARATOR 
-		. $folder . DIRECTORY_SEPARATOR;
-	$touchRandImgUrl = $config['uploads_url'] . DIRECTORY_SEPARATOR 
-		. $folder . DIRECTORY_SEPARATOR;
+  // Set path and url
+  $touchRandImgPath = $config['uploads_path'] . DIRECTORY_SEPARATOR 
+    . $folder . DIRECTORY_SEPARATOR;
+  $touchRandImgUrl = $config['uploads_url'] . DIRECTORY_SEPARATOR 
+    . $folder . DIRECTORY_SEPARATOR;
 
-	if(!is_dir($touchRandImgPath)){
-		mkdir($touchRandImgPath);
-	}
+  if(!is_dir($touchRandImgPath)){
+    mkdir($touchRandImgPath);
+  }
 
-	// Load image files
-	$touchRandImgFiles = scandir($touchRandImgPath);
+  // Load image files
+  $touchRandImgFiles = scandir($touchRandImgPath);
 
-	$c=0; $html=""; shuffle($touchRandImgFiles);
-	foreach($touchRandImgFiles AS $img){
-		if(!is_file($touchRandImgPath . $img)){
-			continue;
-		}
+  $c=0; $html=""; shuffle($touchRandImgFiles);
+  foreach($touchRandImgFiles AS $img){
+    if(!is_file($touchRandImgPath . $img)){
+      continue;
+    }
 
-		$name = array_shift(explode(".",$img,2));
-		$src = $touchRandImgUrl . $img;
+    $name = array_shift(explode(".",$img,2));
+    $src = $touchRandImgUrl . $img;
 
-		if(isset($linkMap[$img])){
-			$html .= "<a class=\"touchRandImgLink\" target=\"$linkTarget\" href=\"" . $linkMap[$img]['url'] . "\">";
-		}	
+    if(isset($linkMap[$img])){
+      $html .= "<a class=\"touchRandImgLink\" target=\"$linkTarget\" href=\"" . $linkMap[$img]['url'] . "\">";
+    }  
 
-		$html .= "<img class=\"touchRandImg\" src=\"$src\" alt=\"$img\" title=\"$name\" />";
+    $html .= "<img class=\"touchRandImg\" src=\"$src\" alt=\"$img\" title=\"$name\" />";
 
-		if(isset($linkMap[$img])){
-			$html .= "</a>";
-		}
+    if(isset($linkMap[$img])){
+      $html .= "</a>";
+    }
 
-		$c++;
-		if($c >= $maxImg){
-			break; 
-		}
-	}
+    $c++;
+    if($c >= $maxImg){
+      break; 
+    }
+  }
 
-	if($c == 0){
-		return $touchRandImgPath . " has no images to randomize...";
-	}
+  if($c == 0){
+    return $touchRandImgPath . " has no images to randomize...";
+  }
 
-	return "\n<!-- touchRandImg plugin -->\n" . $html . "\n<!-- /touchRandImg plugin -->\n";
+  return "\n<!-- touchRandImg plugin -->\n" . $html . "\n<!-- /touchRandImg plugin -->\n";
 }
 
 function linkmap2array($map=array()){
-	$resultMap=array();
-	foreach($map AS $k => $v){
-		$tmp = explode(":",$v,2);
-		$resultMap[$tmp[0]] = array(
-			'url' => $tmp[1]
-		);  
-	}
+  $resultMap=array();
+  foreach($map AS $k => $v){
+    $tmp = explode(":",$v,2);
+    $resultMap[$tmp[0]] = array(
+      'url' => $tmp[1]
+    );  
+  }
 
-	return $resultMap;
+  return $resultMap;
 }
 
 function smarty_cms_help_function_touchrandimg() {
 
-	print "<h3>Usage</h3>";
-	print "<ul>";
-	print "	<li>Basic: {touchrandimg}</li>";
-	print "	<li>Display 3 images: {touchrandimg max_img=3}</li>";
-	print "	<li>Use links: {touchrandimg link_map='imagename01.png:http://www.cmsmadesimple.org,imagename02.png:http://www.touchdesign.de'}</li>";
-	print "	<li>Use link target: {touchrandimg link_map='imagename01.png:http://www.cmsmadesimple.org' link_target='_blank'}</li>";
-	print "</ul>";
+  print "<h3>Usage</h3>";
+  print "<ul>";
+  print "  <li>Basic: {touchrandimg}</li>";
+  print "  <li>Display 3 images: {touchrandimg max_img=3}</li>";
+  print "  <li>Use links: {touchrandimg link_map='imagename01.png:http://www.cmsmadesimple.org,imagename02.png:http://www.touchdesign.de'}</li>";
+  print "  <li>Use link target: {touchrandimg link_map='imagename01.png:http://www.cmsmadesimple.org' link_target='_blank'}</li>";
+  print "</ul>";
 
-	print "<h3>Params</h3>";
-	print "<ul>";
-	print "	<li><em>(optional)</em> folder - Image folder relative to uploads path (default is uploads/touchrandimg/)</li>";
-	print "	<li><em>(optional)</em> max_img - Maximum images to display (default is 1)</li>";
-	print "	<li><em>(optional)</em> link_map - imagename01.png:http://www.cmsmadesimple.org,imagename02.png:http://www.touchdesign.de... (default none)</li>";    
-	print "	<li><em>(optional)</em> link_target - Set target attribute _blank, _self or what ever (default is _self)</li>";
-	print "</ul>";
+  print "<h3>Params</h3>";
+  print "<ul>";
+  print "  <li><em>(optional)</em> folder - Image folder relative to uploads path (default is uploads/touchrandimg/)</li>";
+  print "  <li><em>(optional)</em> max_img - Maximum images to display (default is 1)</li>";
+  print "  <li><em>(optional)</em> link_map - imagename01.png:http://www.cmsmadesimple.org,imagename02.png:http://www.touchdesign.de... (default none)</li>";    
+  print "  <li><em>(optional)</em> link_target - Set target attribute _blank, _self or what ever (default is _self)</li>";
+  print "</ul>";
 
-	smarty_cms_about_function_touchrandimg();
+  smarty_cms_about_function_touchrandimg();
 }
 
 function smarty_cms_about_function_touchrandimg() {
 
-	print "<h3>About</h3>";
-	print "<ul>";
-	print "	<li>Copyright by <a href=\"http://www.touchdesign.de/\">touchDesign</a></li>";
-	print "	<li>Author Christoph Gruber</li>";
-	print "	<li>Support via <a href=\"http://www.homepage-community.de/index.php?topic=1705\">HPC</a></li>";
-	print "	<li>License GPL 2.0</li>";
-	print "	<li>Version 1.0</li>";
-	print "</ul>";
+  print "<h3>About</h3>";
+  print "<ul>";
+  print "  <li>Copyright by <a href=\"http://www.touchdesign.de/\">touchDesign</a></li>";
+  print "  <li>Author Christoph Gruber</li>";
+  print "  <li>Support via <a href=\"http://www.homepage-community.de/index.php?topic=1705\">HPC</a></li>";
+  print "  <li>License GPL 2.0</li>";
+  print "  <li>Version 1.0</li>";
+  print "</ul>";
 }
 
 ?>
